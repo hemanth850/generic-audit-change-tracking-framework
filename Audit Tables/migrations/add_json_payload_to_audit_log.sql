5f30c4e24d4528da83ad5287d00d43f3c3096828
hemanth850 <hemanthga850@gmail.com>
2026-02-09 16:53:22 +0530

    Added json mode.

---
+++ Audit Tables/migrations/add_json_payload_to_audit_log.sql
@@ -0,0 +1,5 @@
+ALTER TABLE audit_log
+ADD (
+    json_payload CLOB
+);
+
--- Reporting View/vw_audit_history.sql
+++ Reporting View/vw_audit_history.sql
@@ -5,6 +5,7 @@ SELECT
     pk_value,
     old_value,
     new_value,
+    json_payload,
     action_type,
     changed_by,
     changed_at
